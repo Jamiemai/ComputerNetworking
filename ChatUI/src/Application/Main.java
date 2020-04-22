c58333508268b698fa25a70f393e97550a188e52
Nguyen Huynh Long <long.nguyenhuynh611@gmail.com>
2020-04-22 08:40:47 +0700

    chuan bi debug foenix

--- ChatUI/src/Application/Main.java
+++ ChatUI/src/Application/Main.java
@@ -7,16 +7,18 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 
 public class Main extends Application {
-
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/FXML/LoginUI.fxml"));
-        primaryStage.setTitle("Seen");
-        primaryStage.setScene(new Scene(root, 600, 400));
-        primaryStage.show();
-        primaryStage.setResizable(false);
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/FXML/LoginUI.fxml"));
+            Scene scene = new Scene(root,600,400);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+            primaryStage.setResizable(false);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
-
 
     public static void main(String[] args) {
         launch(args);
