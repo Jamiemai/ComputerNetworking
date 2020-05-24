d830b471ed0a64e9563bbc5217d0304da7a4c045
Nguyen Huynh Long <long.nguyenhuynh611@gmail.com>
2020-05-24 22:49:44 +0700

    nhan gui nhieu nguoi, onl

--- ChatServer/src/Server/Server.java
+++ ChatServer/src/Server/Server.java
@@ -1,46 +1,43 @@
 package Server;
 
-import DBConnection.DBHandler;
-
 import java.io.*;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.util.*;
 import java.net.*;
 
-public class Server
-{
+public
+class Server {
 
     // Vector to store active clients
     static Vector<ClientHandler> ar = new Vector<>();
 
-    public static void main(String[] args) throws IOException {
-        // server is listening on port 1234
+    public static
+    void main(String[] args) throws IOException {
         ServerSocket ss = new ServerSocket(1234);
 
         Socket s;
 
-        Connection connection;
-        DBHandler handler = null;
-        PreparedStatement pst;
-
-        while (true)
-        {
+        while (true) {
             // Accept the incoming request
             s = ss.accept();
 
             // obtain input and output streams
-            DataInputStream dis = new DataInputStream(s.getInputStream());
+            DataInputStream  dis = new DataInputStream(s.getInputStream());
             DataOutputStream dos = new DataOutputStream(s.getOutputStream());
 
             // Create a new handler object for handling this request.
-            ClientHandler mtch = new ClientHandler(s, "", dis, dos);
+            ClientHandler client = new ClientHandler(s, dis.readUTF(), dis, dos);
+
+            for (ClientHandler clt : Server.ar) { //notify current users new user
+                clt.AddOnlineClient(client.name);
+            }
+
+            client.AddAllOnlineClient();
+
+            // add new client to active clients list
+            ar.add(client);
 
             // Create a new Thread with this object.
-            Thread t = new Thread(mtch);
-
-            // add this client to active clients list
-            ar.add(mtch);
+            Thread t = new Thread(client);
 
             // start the thread.
             t.start();
@@ -49,38 +46,32 @@ public class Server
 }
 
 // ClientHandler class
-class ClientHandler implements Runnable
-{
-    private String name;
+class ClientHandler implements Runnable {
+    public String name;
     final DataInputStream dis;
     final DataOutputStream dos;
     Socket s;
-    boolean online;
 
     // constructor
-    public ClientHandler(Socket s, String name,
-                         DataInputStream dis, DataOutputStream dos) {
+    public
+    ClientHandler(Socket s, String name, DataInputStream dis, DataOutputStream dos) {
         this.dis = dis;
         this.dos = dos;
         this.name = name;
         this.s = s;
-        this.online=true;
     }
 
     @Override
-    public void run() {
+    public
+    void run() {
         String received;
-        while (true)
-        {
-            try
-            {
+        while (true) {
+            try {
                 // receive the string
                 received = dis.readUTF();
 
                 // break the string into message and recipient part
-                StringTokenizer st = new StringTokenizer(received, "#");
-                String MsgToSend = st.nextToken();
-                String recipient = st.nextToken();
+                String[] msgSplit     = received.split("#", 2);
 
                 // search for the recipient in the connected devices list.
                 // ar is the vector storing client of active users
@@ -88,14 +79,15 @@ class ClientHandler implements Runnable
                 {
                     // if the recipient is found, write on its
                     // output stream
-                    if (mc.name.equals(recipient) && mc.online)
+                    if (mc.name.equals(msgSplit[0]))
                     {
-                        mc.dos.writeUTF(this.name+" : "+MsgToSend);
+                        mc.dos.writeUTF(this.name + "#" + msgSplit[1]);
                         break;
                     }
                 }
             } catch (IOException e) {
                 try {
+                    Server.ar.remove(this);
                     this.s.close();
                 } catch (IOException ioException) {
                     ioException.printStackTrace();
@@ -104,4 +96,20 @@ class ClientHandler implements Runnable
 
         }
     }
-} 
+
+    public
+    void AddOnlineClient(String name) throws IOException {
+        this.dos.writeUTF("NEW_USER#" + name);
+    }
+
+    public
+    void AddAllOnlineClient() throws IOException {
+        StringBuilder msg = new StringBuilder("ALL_USER#");
+        for (ClientHandler clt : Server.ar) {
+            msg.append(clt.name).append("#");
+        }
+        if(!msg.toString().equals("ALL_USER#")) { //first user
+            this.dos.writeUTF(msg.toString());
+        }
+    }
+}
